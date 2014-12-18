a958a717545f00b7e2862893cc7b370e7fe05b83
henrymenori <henrymenori@yahoo.com>
2014-12-18 12:04:41 +0700

    Penambahan method user, comment, dan search

--- src/java/service/ControllerImplement.java
+++ src/java/service/ControllerImplement.java
@@ -256,4 +256,169 @@ public class ControllerImplement {
         }
     }
 
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "addUser")
+    public Boolean addUser(@WebParam(name = "username") String username, @WebParam(name = "password") String password, @WebParam(name = "email") String email, @WebParam(name = "role") String role) {
+        
+        Firebase usersRef = ref.child("users");
+        Map<String, String> user = new HashMap();
+        
+        user.put("username", username);
+        user.put("password", password);
+        user.put("email", email);
+        user.put("role", role);
+        
+        usersRef.push().setValue(user);
+        
+        return true;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "editUser")
+    public Boolean editUser(@WebParam(name = "id") String id, @WebParam(name = "username") String username, @WebParam(name = "password") String password, @WebParam(name = "email") String email, @WebParam(name = "role") String role) {
+        
+        Firebase usersRef = ref.child("users/" + id);
+        Map<String, Object> user = new HashMap();
+        
+        user.put("username", username);
+        user.put("password", password);
+        user.put("email", email);
+        user.put("role", role);
+        
+        usersRef.updateChildren(user);
+        
+        return true;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "deleteUser")
+    public Boolean deleteUser(@WebParam(name = "id") String id) {
+        
+        Firebase usersRef = ref.child("users/" + id);
+        
+        usersRef.removeValue();
+        
+        return true;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "loadUser")
+    public List<User> loadUser() {
+        List<User> user = new ArrayList();
+        try {
+            String s = load("https://webservice.firebaseio.com/users.json");
+            JSONObject Obj = new JSONObject(s);
+            Iterator<String> key = Obj.keys();
+
+            while (key.hasNext()) {
+                String id = key.next();
+                JSONObject Obj2 = Obj.getJSONObject(id);
+                User u = new User();
+                u.setId(id);
+                u.setUsername(Obj2.getString("username"));
+                u.setPassword(Obj2.getString("password"));
+                u.setEmail(Obj2.getString("email"));
+                u.setRole(Obj2.getString("role"));
+                user.add(u);
+            }
+            return user;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "addComment")
+    public Boolean addComment(@WebParam(name = "postid") String postid, @WebParam(name = "nama") String nama, @WebParam(name = "email") String email, @WebParam(name = "komentar") String komentar) {
+        
+        Firebase commentsRef = ref.child("comments");
+        Map<String, String> comment = new HashMap();
+        
+        comment.put("postid", postid);
+        comment.put("nama", nama);
+        comment.put("email", email);
+        comment.put("komentar", komentar);
+        
+        commentsRef.push().setValue(comment);
+        
+        return true;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "loadComment")
+    public List<Comment> loadComment(@WebParam(name = "postid") String postid) {
+        List<Comment> comment = new ArrayList();
+        try {
+            String s = load("https://webservice.firebaseio.com/comments.json");
+            JSONObject Obj = new JSONObject(s);
+            Iterator<String> key = Obj.keys();
+
+            while (key.hasNext()) {
+                String id = key.next();
+                JSONObject Obj2 = Obj.getJSONObject(id);
+                if(Obj2.getString("postid").equals(postid)) {
+                    Comment c = new Comment();
+                    c.setId(id);
+                    c.setPostid(Obj2.getString("postid"));
+                    c.setNama(Obj2.getString("nama"));
+                    c.setEmail(Obj2.getString("email"));
+                    c.setKomentar(Obj2.getString("komentar"));
+                    comment.add(c);
+                }
+            }
+            return comment;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "searchPost")
+    public List<Post> searchPost(@WebParam(name = "key") String key) {
+        List<Post> post = new ArrayList();
+        try {
+            String s = load("https://webservice.firebaseio.com/posts.json");
+            JSONObject Obj = new JSONObject(s);
+            Iterator<String> k = Obj.keys();
+
+            while (k.hasNext()) {
+                String id = k.next();
+                JSONObject Obj2 = Obj.getJSONObject(id);
+                String test = Obj2.getString("judul") + Obj2.getString("konten");
+                if(test.toLowerCase().contains(key)) {
+                    Post p = new Post();
+                    p.setId(id);
+                    p.setJudul(Obj2.getString("judul"));
+                    p.setTanggal(Obj2.getString("tanggal"));
+                    p.setKonten(Obj2.getString("konten"));
+                    p.setStatus(Obj2.getString("status"));
+                    post.add(p);
+                }
+            }
+            return post;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
